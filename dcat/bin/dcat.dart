6f75754be10645b02237882ceed4ae2488d850c9
SolaceDev <20241120211@stu.ynu.edu.cn>
2026-05-18 14:03:17 +0800

    更新

--- dcat/bin/dcat.dart
+++ dcat/bin/dcat.dart
@@ -7,6 +7,7 @@ import 'package:args/args.dart';
 const lineNumber = 'line-number';
 
 void main(List<String> arguments) {
+  //https://dart.ac.cn/tutorials/server/cmdline#parsing-command-line-arguments
   exitCode = 0; // Presume success
   final parser = ArgParser()..addFlag(lineNumber, negatable: false, abbr: 'n');
 
